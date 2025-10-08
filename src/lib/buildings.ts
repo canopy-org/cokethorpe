7f9cfed9363bde8f84c3d443f4b09b19e01dba53
tomarnison <61083788+tomarnison@users.noreply.github.com>
2025-10-08 20:50:49 +0100

    improved energy usage stats3

--- src/lib/buildings.ts
+++ src/lib/buildings.ts
@@ -87,7 +87,7 @@ export const buildings: Building[] = [
         name: 'Mansion House - Main',
         type: 'EM300-DI',
         location: 'Main Hall',
-        conversionFactor: 10
+        conversionFactor: 1
       },
     ],
     primarySensors: {
